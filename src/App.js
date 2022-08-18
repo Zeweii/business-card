da2ba97b0397d82af025884e76cad7bae1541232
Zachary Jiang Mac <zacharyjiang720@gmail.com>
2022-08-18 14:27:16 +1000

    added media-footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React from "react"
 import './App.css';
 
 export default function App() {
-  return (
-    <div className="App">
-        <div className="box">
-            Box
-        </div>
-    </div>
-  );
+        return (
+                <div className="App">
+                        <div className="box">
+                                <img className="image" src="../public/photo.png" />
+                        
+                        <div className="description-box" >
+                        </div>      
+
+                        <div className="media-footer">
+                                
+                        </div>
+
+
+                        </div>
+                </div>
+        );
 }
+    
