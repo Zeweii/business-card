a3b3409727d9af6f24ff8c723d1eaa1ec84df287
Zachary Jiang Mac <zacharyjiang720@gmail.com>
2022-08-23 22:52:59 +1000

    Added name and title card

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ export default function App() {
                                 <img className="image" src="/photo.png" />
                 
                                 <div className="description-box" >
+                                        <h3 className="card-name">Zachary Jiang</h3>
+                                        <h5 className="card-occupation">Software Developer</h5>
                                 </div>      
 
                                 <div className="media-footer">
